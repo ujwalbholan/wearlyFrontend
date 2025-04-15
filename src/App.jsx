19337d62711f6be2885c168e4721012d77bd78ba
ujwalbholan <ujwal.bholan022@apexcollege.edu.np>
2025-04-15 15:53:14 +0545

    added random paragraph for checking

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,45 @@ function App() {
     <>
       <div className="p-1.5 flex flex-col justify-center items-center gap-2">
         <h1 className="font-bold ">welcome to wearly</h1>
-        <Button className='bg-gray-700 shadow-2xl rounded-b-md text-white p-2 '>click me</Button>
+        <p className="text-justify ml-2 mr-2 p-3">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni a
+          autem, facere aliquid doloribus labore qui nemo molestias fugiat nulla
+          minima commodi iure accusamus eveniet natus aperiam omnis ut animi sit
+          iusto totam iste tenetur? Nulla laudantium maxime voluptas porro
+          doloremque vel corporis! Sapiente, ut dolorem minus provident sed
+          repellendus beatae quae possimus velit earum dicta optio placeat nihil
+          ipsum necessitatibus similique nostrum laboriosam expedita iste cumque
+          aspernatur! Assumenda error ad deserunt optio eius minima alias
+          beatae, neque voluptates aperiam? Incidunt deleniti consequatur
+          aliquam minima odio neque nulla unde pariatur repellat magni ullam vel
+          asperiores nobis, at veritatis non quisquam, fugit ea similique eos
+          doloribus qui perspiciatis saepe? Nostrum pariatur qui quia, unde
+          repellat libero consequatur, enim sunt ut ex veniam provident
+          delectus, beatae maxime? Ipsa quo sapiente laudantium animi, saepe
+          iste nemo, veniam ab aspernatur eum nam voluptatem? Qui modi laborum
+          deserunt reprehenderit, odit laudantium dolorem quidem amet deleniti?
+          Suscipit ipsa accusantium nostrum aliquam beatae laboriosam deleniti
+          officia veritatis, natus mollitia dolorum molestiae. Consequuntur
+          praesentium quam laudantium atque exercitationem cum impedit illum in
+          voluptate. Hic non repudiandae sit, minus adipisci deserunt ratione
+          amet a blanditiis odit ducimus aspernatur sequi laborum aut dicta
+          officia iusto numquam consequatur nam voluptatum recusandae architecto
+          ad nobis. Deserunt dolorem delectus eius eligendi excepturi. Ratione
+          ipsa laborum assumenda quasi est, blanditiis, amet iure nisi quibusdam
+          suscipit placeat. Adipisci illum nisi rem, neque voluptate amet
+          suscipit dignissimos dicta incidunt consectetur dolore quo perferendis
+          id sed voluptatum quasi perspiciatis ut eius pariatur dolorum esse sit
+          magni repudiandae temporibus. Debitis, fuga praesentium beatae
+          obcaecati consequuntur consectetur fugit eaque perferendis hic
+          voluptas ipsa porro consequatur magnam soluta enim nulla nesciunt
+          eius. Odio fugiat atque aspernatur deleniti, ad deserunt. Cupiditate
+          corporis nihil laudantium asperiores corrupti alias exercitationem.
+          Dolorem dignissimos, illo fuga quas architecto modi soluta tempora,
+          atque, odit quibusdam quod.
+        </p>
+        <Button className="bg-gray-700 shadow-2xl rounded-b-md text-white p-2 ">
+          click me
+        </Button>
       </div>
     </>
   );
